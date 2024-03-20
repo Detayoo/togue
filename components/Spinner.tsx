f6531a635b5a39950ac7913d9fb805c81f5ed6a8
Tayo <teeydigba@gmail.com>
2024-03-20 16:52:20 +0100

    added words to spinner

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,7 +1,10 @@
 export const Spinner = ({ className }: { className?: string }) => {
   return (
-    <div className={`h-[80vh] w-full flex justify-center items-center ${className}`}>
+    <div
+      className={`h-[80vh] w-full flex flex-col justify-center items-center ${className}`}
+    >
       <div className="spinner" />
+      <p className="mt-1">Just one second..</p>
     </div>
   );
 };
