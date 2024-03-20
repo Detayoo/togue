f77ade3b5dc4ee0dc4b6e208f88fbdc84491ead2
Tayo <teeydigba@gmail.com>
2024-03-20 09:51:16 +0100

    completed header and started the body

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import { Header } from "@/components";
 export default function Home() {
   return (
     <div className="w-full">
-      <div className="max-w-[1300px] mx-auto text-white px-10 m:px-0">
+      <div className="max-w-[1300px] mx-auto text-white px-10 m:px-0 h-screen">
         <Header />
+        <div className="h-[90vh] overflow-y-auto m:px-10 py-5 text-sm"></div>
       </div>
     </div>
   );
